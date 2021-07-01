68fd706ab7264f32ecc96afa3950e6a73ae7d9f9
Rudicel <r.figueroa.s.94@gmail.com>
2021-06-30 21:59:00 -0500

    Se creo la estructura basica y se agrego bootstrap

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"\>
+        <title>¡Bienvenido a Fandub Varios!</title>
+    </head>
+    <body>
+        Aquí va el contenido de la web!!!
+    </body>
+</html>
